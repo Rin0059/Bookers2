2cb05196ed97e0396a6401d75b46780e54e06f99
Rin0059 <fandorin0059@gmail.com>
2021-05-21 10:32:58 +0000

    [update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,35 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    end
+  end
+
   def index
+    @book = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @users = @book.users
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:shop_name, :image, :caption)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def show
+
+  def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
