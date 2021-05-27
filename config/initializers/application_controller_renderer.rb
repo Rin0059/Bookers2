e409057049aed445d540433cb3d2a71ac0521dc7
Rin0059 <fandorin0059@gmail.com>
2021-05-27 07:58:41 +0000

    [Finish]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '41d51b83f80b1e2673411e1c282dce47648320011b114fa2e8a567d6fa2c0e86eb53847611e196d02427d540d945c3510e27f4df3b28fb06d9be54dd03db73bb'
